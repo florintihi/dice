10792ef7af44c53ad84559d25b02f99e1c2737f7
Tihi Florin <tihi.florin66@gmail.com>
2024-01-08 14:28:05 +0200

    3rd

--- js/script.js
+++ js/script.js
@@ -25,6 +25,14 @@ function handleClick() {
       location.replace("https://www.youtube.com/watch?v=YVDKnQLz0bk");
     }, 2000);
   }
+
+  if (dice[0] === 6 && dice[1] === 5) {
+    wait.textContent = "Wait for it!🎲";
+    rollButton.removeEventListener("click", handleClick);
+    setTimeout(function redirect() {
+      location.replace("https://www.youtube.com/watch?v=3v13fH71lkk");
+    }, 2000);
+  }
 }
 
 rollButton.addEventListener("click", handleClick);
